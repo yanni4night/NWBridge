32f05f4259a08c9819b2ec7c819cce330f15b252
yinyong02 <yinyong02@baidu.com>
2015-11-20 23:03:15 +0800

    new base apis

--- src/api.js
+++ src/api.js
@@ -11,10 +11,69 @@
  */
 
 var apis = {
-    location: {},
-    localStorage: {},
+    location: {
+        href: function () {
+            return location.href;
+        },
+        host: function () {
+            return location.host;
+        },
+        hostname: function () {
+            return location.hostname;
+        },
+        pathname: function () {
+            return location.pathname;
+        },
+        port: function () {
+            return location.port;
+        },
+        origin: function () {
+            return location.origin;
+        },
+        search: function () {
+            return location.search;
+        },
+        hash: function () {
+            return location.hash;
+        },
+        reload: function (newUrl) {
+            return location.reload(newUrl);
+        },
+        assign: function (newUrl) {
+            return location.assign(newUrl);
+        },
+        replace: function (newUrl) {
+            return location.replace(newUrl);
+        }
+    },
+    localStorage: {
+        enabled: function () {
+            if ('undefined' === typeof localStorage) {
+                return false;
+            }
+            localStorage.setItem('__test', 1);
+            return 1 === +localStorage.getItem('__test');
+        },
+        setItem: function (key, value) {
+            localStorage.setItem(key, value);
+        },
+        getItem: function (key) {
+            localStorage.getItem(key);
+        },
+        removeItem: function (key) {
+            localStorage.removeItem(key);
+        }
+    },
+    cookie: {
+        enabled: function () {
+            return navigator.cookieEnabled;
+        },
+        setItem: function () {},
+        getItem: function () {},
+        removeItem: function () {}
+    },
     navigator: {
-        getUserAgent: function() {
+        getUserAgent: function () {
             return navigator.userAgent;
         }
     }
@@ -22,11 +81,11 @@ var apis = {
 
 export function Api(cmd, method, data) {
 
-    this.exists = function() {
+    this.exists = function () {
         return apis[cmd] && 'function' === typeof apis[cmd][method];
     };
 
-    this.invoke = function() {
+    this.invoke = function () {
         if (!this.exists()) {
             throw new Error('"' + cmd + '.' + method + '" does not exist');
         }
@@ -36,7 +95,7 @@ export function Api(cmd, method, data) {
 
 }
 
-Api.register = function(cmd, method, func) {
+Api.register = function (cmd, method, func) {
     apis[cmd] = apis[cmd] || {};
     if (apis[cmd][method]) {
         throw new Error('Duplicated "' + cmd + '.' + method + '"')
