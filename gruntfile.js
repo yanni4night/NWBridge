8f2d2a7eb323de89dc78f7b6b8e7b7aa80a7d926
yinyong02 <yinyong02@baidu.com>
2015-11-22 22:21:27 +0800

    remove comment

--- gruntfile.js
+++ gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function (grunt) {
         },
         babel: {
             options: {
+                comments: false,
                 presets: ['babel-preset-es2015']
             },
             es2015: {
